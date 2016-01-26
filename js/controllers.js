adfd75738f2bd977a3e1c97b5563e9ef64298617
Ariel Araza <ariel.araza@gmail.com>
2016-01-25 22:06:44 -0500

    made controller.js work in non-chrome browsers by removing the fat arrow syntax

--- js/controllers.js
+++ js/controllers.js
@@ -3,18 +3,17 @@ var phonecatApp = angular.module('myApp', []);
 phonecatApp.controller('ChemicalsCtrl', function ($scope, $http) {
   $scope.chems = [];
 
-  $scope.find = () => {
+  $scope.find = function () {
 
-    $http.get(`/l?q=${ $scope.search_str}`).then((result) => {
+    $http.get(`/l?q=${ $scope.search_str}`).then(function (result) {
       result = result.data;
       if (result.truncated) {
-          $scope.results_count = `Too many matches (${result.count}). Showing on first 1000 records`;
+          $scope.results_count = "Too many matches (" +result.count + "). Showing on first 1000 records";
       } else {
           $scope.results_count = result.count;
       }
       $scope.chems = result.records;
     });
-
 
   }
 
--- queryjson.js
+++ queryjson.js
@@ -2,12 +2,13 @@ var Datastore = require('nedb');
 var db = new Datastore({filename: __dirname + '/db.js', autoload: true});
 
 var _ = require("lodash");
+
 var LIMIT = 1000;
 
 function find(str, cb) {
-    console.log("received query", str)
+    console.log("received query", str);
     var r = new RegExp(str, "i");
-    db.find({name: r}, (err, res) => {
+    db.find({$or: [{name: r}, {cas_no: r}]}, (err, res) => {
         if (res.length > LIMIT) {
             cb({records: res.slice(0, LIMIT), truncated: true, count: res.length})
         } else {
